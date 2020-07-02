a1015ab62bad7819134cf9f4e24c3fa8d8c121ad
yehric2018 <eric.yehj@outlook.com>
2020-07-01 19:49:57 -0700

    consensussequence class and presentation.md

---
+++ doc/presentation.md
@@ -0,0 +1,11 @@
+# Poster presentation
+## Background
+text goes here
+## Goals/Questions
+text goes here
+## Design
+text goes here
+## Future Work
+text goes here
+## References/Acknowledgements
+text goes here
--- src/generate_alignments.py
+++ src/generate_alignments.py
@@ -24,6 +24,8 @@ AUTHOR(S):
 import argparse
 import os
 
+DIV_VALUES = [14, 18, 20, 25, 30]
+
 def splitConsensus(fa_file):
     """
     splitConsensus(fa_file): Opens the given fa_file and separates
@@ -52,10 +54,59 @@ def splitConsensus(fa_file):
     line = f.readline()
     while line != "":
         if line[0] == ">":
+            if g != None:
+                g.close()
             g = open(os.path.join(dir_name,
                     line[1:].split()[0] + ".fa"), "a")
         g.write(line)
         line = f.readline()
+    f.close()
+    return dir_name
+
+class ConsensusSequence:
+    """
+    Stores information on a given consensus sequence, such as the
+    sequence name, average kimura divergence, and the sequence
+    itself.
+    """
+    def __init__(self, fa_file):
+        """
+        Initializes this ConsensusSequence with the given parameters.
+        Retrieves the name and divergence from the given fa file.
+
+        The first line should be in the following format:
+        >DF######## avg_kimura=#### [name]
+        The first token in this line will be the name of this
+        consensus and the avg_kimura will be the divergence,
+        rounded to the nearest divergence value in DIV_VALUES.
+
+        Args:
+            fa_file - name of the consensus fa file following the fa
+            format.
+        """
+        f = open(fa_file, "r")
+        lines = f.readlines()
+        description = lines[0][1:].split()
+        self.name = description[0]
+        self.seq = "".join([x[:-1] for x in lines[1:]])
+        self.__setDivergence__(float(description[1].split("=")[1]))
+        f.close()
+
+    def __setDivergence__(self, div):
+        """
+        Helper function called by __init__ to set self.divergence.
+
+        Args:
+            div - avg kimura divergence from fa file.
+        """
+        minDist = 100.0
+        divVal = -1
+
+        for v in DIV_VALUES:
+            if abs(v - div) < minDist:
+                minDist = abs(v - div)
+                divVal = v
+        self.divergence = divVal
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -63,4 +114,10 @@ if __name__ == '__main__':
             help="path to the fa_file containing consensus sequences")
     args = parser.parse_args()
 
-    splitConsensus(args.fa_file)
+    dir_name = splitConsensus(args.fa_file)
+
+    filelist = [ f for f in os.listdir(dir_name) ]
+    for f in filelist:
+        cs = ConsensusSequence(os.path.join(dir_name, f))
+        print(cs.name + ": " + str(cs.divergence))
+        print(cs.seq + "\n")
