b95ac211c5e7690d234f10d4f4d18c867de33614
yehric2018 <eric.yehj@outlook.com>
2020-07-08 22:21:34 -0700

    some more regex parsing

--- src/score_thresholds.py
+++ src/score_thresholds.py
@@ -6,9 +6,6 @@ sequence such that the false discovery rate is below 0.2%. Uses
 empirical and theoretical FDR calculations and chooses the more
 conservative score threshold value for each GC background.
 
-Usage:
-Args:
-
 AUTHOR(S):
     Eric Yeh
 """
@@ -17,6 +14,42 @@ AUTHOR(S):
 # Module imports
 #
 import re
+
+def readScoresFromFile(sc_file):
+    """
+    readScoresFromFile(sc_file) - Reads the given alignment file and
+    produces a list containing for each alignment the score and the
+    lengths of the overlapping sequences, sorted in decreasing order
+    by score.
+
+    Args:
+        sc_file - path to alignment file produced from RMBlast.
+
+    Returns: list of Hit objects produced from sc_file sorted
+        in decreasing order by score.
+    """
+    f = open(sc_file, "r")
+    scoreRegex = re.compile(r"^\s*(\d+)\s+\d+\.\d+\s+\d+\.\d+")
+    rangeRegex1 = re.compile(r"\s*(\d+)\s+(\d+)\s+\(\d+\)\s+\S+\s+(\d+)\s+(\d+)\s+\(\d+\)$")
+    rangeRegex2 = re.compile(r"\s*(\d+)\s+(\d+)\s+\(\d+\)\s+C\s+\S+\s+\(\d+\)\s+(\d+)\s+(\d+)$")
+    hits = []
+    line = f.readline()
+    while line != "":
+        mo = scoreRegex.search(line)
+        if mo:
+            hits.append(int(mo.group(1)))
+        mo = rangeRegex1.search(line)
+        if mo:
+            print(mo)
+            print(int(mo.group(2)) - int(mo.group(1)))
+        mo = rangeRegex2.search(line)
+        if mo:
+            print(mo)
+            print(int(mo.group(3)) - int(mo.group(4)))
+        line = f.readline()
+    hits.sort(reverse=True)
+    print(hits)
+    return hits
 
 def generateScoreThreshold(genome_file, benchmark_file):
     """
@@ -33,20 +66,8 @@ def generateScoreThreshold(genome_file, benchmark_file):
     Returns: score threshold for this consensus sequence computed
         from the given alignments.
     """
-    genomic_hits = []
-    benchmark_hits = []
-    gf = open(genome_file, "r")
-    bf = open(benchmark_file, "r")
-    regex = re.compile(r"^\s*(\d+)\s+\d+\.\d+\s+\d+\.\d+")
-    count = 0
-    line = gf.readline()
-    while line != "":
-        mo = regex.search(line)
-        if mo:
-            count += 1
-            genomic_hits.append(int(mo.group(1)))
-        line = gf.readline()
-    print(genomic_hits)
+    genomic_hits = readScoresFromFile(genome_file)
+    benchmark_hits = readScoresFromFile(benchmark_file)
 
 if __name__ == '__main__':
     generateScoreThreshold("../results/test_alignments/dfamseq/DF0000001_25p35g.sc",
