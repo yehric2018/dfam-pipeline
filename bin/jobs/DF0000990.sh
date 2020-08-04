73963a39eedbc442624f5151a571743c6dfe3473
yehric2018 <eric.yehj@outlook.com>
2020-08-04 13:42:28 -0700

    cleaning bash scripts from repo

--- bin/jobs/DF0000990.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --partition wheeler_lab_large_cpu
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=6
-#SBATCH --job-name=DF0000990
-#SBATCH --output=%x-slurm-%j.out
-#SBATCH --error=%x-slurm-%j.err
-
-python3 ../src/run_job.py ../data/consensus/Dfam_HG38_Families.fa_/DF0000990.fa
--- bin/jobs/DF0001090.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --partition wheeler_lab_large_cpu
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=6
-#SBATCH --job-name=DF0001090
-#SBATCH --output=%x-slurm-%j.out
-#SBATCH --error=%x-slurm-%j.err
-
-python3 ../src/run_job.py ../data/consensus/Dfam_HG38_Families.fa_/DF0001090.fa
